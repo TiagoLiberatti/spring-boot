5048a4267397be4de59a935d4705f0fa8dd2a67c
Tiago Liberatti <tiagoliberatti@gmail.com>
2020-11-24 16:00:39 -0300

    criando repository e service para categoria

---
+++ src/main/java/com/tiagodev/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.tiagodev.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tiagodev.cursomc.domain.Categoria;
+import com.tiagodev.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> categoria = repo.findById(id);
+		return categoria.orElse(null);
+	}
+}
