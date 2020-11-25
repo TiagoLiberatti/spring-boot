768bd27bfa89db4879ca3533ad7f93c10a6e4974
Tiago Liberatti <tiagoliberatti@gmail.com>
2020-11-25 14:18:12 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/tiagodev/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.tiagodev.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tiagodev.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+
+}
