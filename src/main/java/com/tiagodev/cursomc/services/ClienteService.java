73e1e547969732cab844a80232a6d1d770a94073
Tiago Liberatti <tiagoliberatti@gmail.com>
2020-11-26 12:35:08 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/tiagodev/cursomc/services/ClienteService.java
+++ src/main/java/com/tiagodev/cursomc/services/ClienteService.java
@@ -10,9 +10,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.tiagodev.cursomc.domain.Cidade;
 import com.tiagodev.cursomc.domain.Cliente;
+import com.tiagodev.cursomc.domain.Endereco;
+import com.tiagodev.cursomc.domain.enums.TipoCliente;
 import com.tiagodev.cursomc.dto.ClienteDTO;
+import com.tiagodev.cursomc.dto.ClienteNewDTO;
 import com.tiagodev.cursomc.repositories.ClienteRepository;
+import com.tiagodev.cursomc.repositories.EnderecoRepository;
 import com.tiagodev.cursomc.services.exceptions.DataIntegrityException;
 import com.tiagodev.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -21,6 +26,8 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -56,8 +63,31 @@ public class ClienteService {
 		return new Cliente(dto.getId(), dto.getNome(), dto.getEmail(), null, null);
 	}
 	
+	public Cliente fromDto(ClienteNewDTO dto) {
+		Cliente cliente = new Cliente(null, dto.getNome(), dto.getEmail(), dto.getCpfOuCnpj(), TipoCliente.toEnum(dto.getTipo()));
+		Cidade cidade = new Cidade(dto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, dto.getLogradouro(), dto.getNumero(), dto.getComplemento(), dto.getBairro(), dto.getCep(), cliente, cidade);
+		cliente.getEnderecos().add(end);
+		cliente.getTelefones().add(dto.getTelefone1());
+		if(dto.getTelefone2() != null) {
+			cliente.getTelefones().add(dto.getTelefone2());
+		}
+		if(dto.getTelefone3() != null) {
+			cliente.getTelefones().add(dto.getTelefone3());
+		}
+		return cliente;
+	}
+	
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;		
+	}
+	
 }
