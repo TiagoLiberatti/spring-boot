c4d66a0695aed0098d8edcb316cabcba753dd670
Tiago Liberatti <tiagoliberatti@gmail.com>
2020-11-26 12:11:12 -0300

    Correções em CategoriaService e construtor de pagamento

--- src/main/java/com/tiagodev/cursomc/services/CategoriaService.java
+++ src/main/java/com/tiagodev/cursomc/services/CategoriaService.java
@@ -38,8 +38,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);		
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);		
 	}
 
 	public void delete(Integer id) {
@@ -59,6 +60,10 @@ public class CategoriaService {
 	
 	public Categoria fromDto(CategoriaDTO dto) {
 		return new Categoria(dto.getId(), dto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 	
 }
